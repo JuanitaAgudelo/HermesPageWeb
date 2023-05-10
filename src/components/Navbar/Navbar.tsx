008d4635b06cf8c6191cec5210b6eb0059206c24
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-05-10 15:20:03 -0500

    Nabvar integration

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,8 +3,18 @@ import styles from "./Navbar.module.css"
 
 
 export default function Navbar() {
+
+
     return (
-    <nav className={styles.navbar__container}>Navbar</nav>
+    <nav className={styles.navbar__container}>
+            <ul className={styles.items}>
+                <li className={styles.item} >Inicio</li>
+                <li className={styles.item} >Galeria</li>
+                <li className={styles.item} >Conocenos</li>
+                <li className={styles.item} >Calendario</li>
+                <li className={styles.item} >Formulario de contacto</li>
+            </ul>
+    </nav>
 
   )
 }
