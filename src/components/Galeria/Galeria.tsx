e0e7c657fa39f79a90454ab5350c5b1ce80e40b7
Luis_dev <73004377+Another-DevX@users.noreply.github.com>
2023-05-10 19:18:30 -0500

    setup gallery

--- src/components/Galeria/Galeria.tsx
+++ src/components/Galeria/Galeria.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from "react";
+import styles from "./Galeria.module.css"
 
 export default function Galeria() {
   return (
-    <div>Galeria</div>
-  )
+    <section id="Galeria" className={styles.gallery}>
+      <h1>Galeria</h1>
+      <p>Estos son solo algunos de los eventos que hemos llevado a cabo</p>
+      <div className={styles.gallery__container}>
+        <figure className={styles.img_container} >
+          <img className={styles.image} alt="Imagen de eventos pasados de hermes" src="src\assets\images\Hermes_image_1.jpg" height={"100%"} width={"100%"} />
+        </figure>
+        <figure className={styles.img_container} >
+          <img className={styles.image} alt="Imagen de eventos pasados de hermes" src="src\assets\images\Hermes_image_1.jpg" height={"100%"} width={"100%"} />
+        </figure>
+        <figure className={styles.img_container} >
+          <img className={styles.image} alt="Imagen de eventos pasados de hermes" src="src\assets\images\Hermes_image_1.jpg" height={"100%"} width={"100%"} />
+        </figure>
+        <figure className={styles.img_container} >
+          <img className={styles.image} alt="Imagen de eventos pasados de hermes" src="src\assets\images\Hermes_image_1.jpg" height={"100%"} width={"100%"} />
+        </figure>
+        <figure className={styles.img_container} >
+          <img className={styles.image} alt="Imagen de eventos pasados de hermes" src="src\assets\images\Hermes_image_1.jpg" height={"100%"} width={"100%"} />
+        </figure>
+        <figure className={styles.img_container} >
+          <img className={styles.image} alt="Imagen de eventos pasados de hermes" src="src\assets\images\Hermes_image_1.jpg" height={"100%"} width={"100%"} />
+        </figure>
+        <figure className={styles.img_container} >
+          <img className={styles.image} alt="Imagen de eventos pasados de hermes" src="src\assets\images\Hermes_image_1.jpg" height={"100%"} width={"100%"} />
+        </figure>
+        <figure className={styles.img_container} >
+          <img className={styles.image} alt="Imagen de eventos pasados de hermes" src="src\assets\images\Hermes_image_1.jpg" height={"100%"} width={"100%"} />
+        </figure>
+        <figure className={styles.img_container} >
+          <img className={styles.image} alt="Imagen de eventos pasados de hermes" src="src\assets\images\Hermes_image_1.jpg" height={"100%"} width={"100%"} />
+        </figure>
+      </div>
+    </section>
+  );
 }
--- src/pages/homepage/homepage.tsx
+++ src/pages/homepage/homepage.tsx
@@ -2,11 +2,13 @@ import React from 'react'
 import MainScreen from '../../components/MainScreen/MainScreen'
 import Navbar from '../../components/Navbar/Navbar'
 import styles from "./homepage.module.css"
+import Galeria from '../../components/Galeria/Galeria'
 export default function Homepage() {
   return (
-    <>
+    <div style={{height:"200vh"}}>
         <Navbar/>
         <MainScreen/>
-    </>
+        <Galeria/>
+    </div>
   )
 }
